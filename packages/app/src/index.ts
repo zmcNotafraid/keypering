4714f5d9632b920c89e2c0f60ca9591fe9b01650
Keith <keithwhisper@gmail.com>
2020-07-22 16:12:54 +0800

    chore: enable electron builder

---
+++ packages/app/src/index.ts
@@ -0,0 +1,16 @@
+import { app, BrowserWindow } from 'electron'
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: false
+    }
+  })
+
+  win.loadURL('http://localhost:3000')
+}
+
+app.whenReady().then(createWindow)
+
