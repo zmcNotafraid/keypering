9c4d3eadf4c169eea4cb4b5e2e33d459524e1c25
duanyytop <duanyytop@gmail.com>
2020-07-29 18:08:15 +0800

    feat: Add main header component

---
+++ packages/ui/src/components/MainHeader/index.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react'
+import styles from './mainHeader.module.scss'
+import { getWalletIndex } from '../../services/channels'
+import { useHistory } from 'react-router'
+import { Channel } from '@keypering/specs'
+import { isSuccessResponse, Routes } from '../../utils'
+import WalletManager from '../WalletManager'
+
+const MainHeader = () => {
+  const [currentWallet, setCurrentWallet] = useState<Channel.WalletProfile>({ name: '', id: '', xpub: '' })
+  const [showWalletManager, setShowWalletManager] = useState(false)
+  const history = useHistory()
+
+  const toggleWalletManager = () => setShowWalletManager(!showWalletManager)
+
+  useEffect(() => {
+    getWalletIndex()
+      .then(res => {
+        if (isSuccessResponse(res)) {
+          const { current, wallets } = res.result
+          if (current && wallets.length > 0) {
+            setCurrentWallet(wallets.filter((wallet: Channel.WalletProfile) => wallet.id === current)[0])
+          } else {
+            history.push(Routes.Welcome)
+          }
+        }
+      })
+      .catch(() => {
+        history.push(Routes.Welcome)
+      })
+  }, [])
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.nav}>
+        <h1>{currentWallet.name}</h1>
+        <button onClick={toggleWalletManager}>Manager</button>
+        <WalletManager show={showWalletManager} setShow={setShowWalletManager} />
+      </div>
+    </div>
+  )
+}
+
+export default MainHeader
--- packages/ui/src/containers/Main/index.tsx
+++ packages/ui/src/containers/Main/index.tsx
@@ -1,46 +1,15 @@
-import React, { useState } from 'react'
+import React from 'react'
 import AuthList from '../../components/AuthList'
-import { useEffect } from 'react'
-import { useHistory } from 'react-router-dom'
 import styles from './main.module.scss'
-import { getWalletIndex } from '../../services/channels'
-import { isSuccessResponse, Routes } from '../../utils'
-import { Channel } from '@keypering/specs'
-import WalletManager from '../../components/WalletManager'
+import MainHeader from '../../components/MainHeader'
 
 const Main = () => {
-  const [currentWallet, setCurrentWallet] = useState<Channel.WalletProfile>({ name: '', id: '', xpub: '' })
-  const [showWalletManager, setShowWalletManager] = useState(false)
-  const history = useHistory()
-
-  const toggleWalletManager = () => setShowWalletManager(!showWalletManager)
-
-  useEffect(() => {
-    getWalletIndex()
-      .then(res => {
-        if (isSuccessResponse(res)) {
-          const { current, wallets } = res.result
-          if (current && wallets.length > 0) {
-            setCurrentWallet(wallets.filter((wallet: Channel.WalletProfile) => wallet.id === current)[0])
-          } else {
-            history.push(Routes.Welcome)
-          }
-        }
-      })
-      .catch(() => {
-        history.push(Routes.Welcome)
-      })
-  }, [])
-
   return (
     <div className={styles.container}>
-      <h1>{currentWallet.name}</h1>
+      <MainHeader />
       <div>
         <AuthList />
       </div>
-
-      <button onClick={toggleWalletManager}>Wallet Manager</button>
-      <WalletManager show={showWalletManager} setShow={setShowWalletManager} />
     </div>
   )
 }
