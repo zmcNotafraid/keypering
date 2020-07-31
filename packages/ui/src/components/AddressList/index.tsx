cbe47c4b28eb50b591675df0c08f433ce6b0ab97
duanyytop <duanyytop@gmail.com>
2020-07-31 16:55:12 +0800

    chore: Format address list page

--- packages/ui/src/components/AddressList/index.tsx
+++ packages/ui/src/components/AddressList/index.tsx
@@ -4,6 +4,22 @@ import { getAddressList } from '../../services/channels'
 import { isSuccessResponse } from '../../utils'
 import styles from './addressList.module.scss'
 
+const adaptAddress = (value: string) => {
+  if (value === undefined || value === null) return ''
+  if (value.length <= 40) return value
+  return `${value.substr(0, 20)}...${value.substr(value.length - 20, 20)}`
+}
+
+const copyAddress = (address: string) => {
+  const element = document.createElement('textarea')
+  element.value = address
+  document.body.appendChild(element)
+  element.select()
+  document.execCommand('copy')
+  document.body.removeChild(element)
+  alert('Copied')
+}
+
 const AddressList = () => {
   const [list, setList] = useState<Channel.Address[]>([])
   useEffect(() => {
@@ -18,17 +34,19 @@ const AddressList = () => {
     <div className={styles.container}>
       {list.map(address => (
         <div key={address.address} className={styles.item}>
-          <span className={styles.tag}>{address.tag}</span>
-          <span className={styles.address}>{address.address}</span>
+          <span className={styles.tag}>{adaptAddress(address.tag)}</span>
+          <div className={styles.address} id="address" onClick={() => copyAddress(address.address)}>
+            {adaptAddress(address.address)}
+          </div>
           <div className={styles.capacity}>
             <div>
               <span>Free</span>
-              <span>{address.free}</span>
+              <span>{(address.free / 10 ** 8).toFixed(8)}</span>
             </div>
             <span className={styles.separate} />
             <div>
               <span>In Use</span>
-              <span>{address.inUse}</span>
+              <span>{(address.inUse / 10 ** 8).toFixed(8)}</span>
             </div>
           </div>
         </div>
