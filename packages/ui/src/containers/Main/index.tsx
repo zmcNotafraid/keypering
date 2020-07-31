3ff5f1a1fa6473f9364ff2543d322f8d744489f3
Keith <keithwhisper@gmail.com>
2020-07-31 15:49:38 +0800

    feat: update style of lists

--- packages/ui/src/containers/Main/index.tsx
+++ packages/ui/src/containers/Main/index.tsx
@@ -1,19 +1,57 @@
-import React from 'react'
+import React, { useMemo } from 'react'
+import { NavLink, useParams } from 'react-router-dom'
 import MainHeader from '../../components/MainHeader'
 import AuthList from '../../components/AuthList'
 import TxList from '../../components/TxList'
 import AddressList from '../../components/AddressList'
+import { Routes } from '../../utils'
 import styles from './main.module.scss'
 
-const Main = () => (
-  <div className={styles.container}>
-    <MainHeader />
-    <div>
-      <AddressList />
-      <TxList />
-      <AuthList />
+enum Tab {
+  Address = 'address',
+  Transaction = 'transaction',
+  Authorization = 'authorization',
+}
+
+const Main = () => {
+  const { tab: active = Tab.Address } = useParams<{ tab?: Tab }>()
+  const list = useMemo(() => {
+    switch (active) {
+      case Tab.Address: {
+        return <AddressList />
+      }
+      case Tab.Transaction: {
+        return <TxList />
+      }
+      case Tab.Authorization: {
+        return <AuthList />
+      }
+      default: {
+        return null
+      }
+    }
+  }, [active])
+  return (
+    <div className={styles.container}>
+      <MainHeader />
+      <div className={styles.balance}>
+        <span>Balance: xxx</span>
+        <button type="button">Send</button>
+      </div>
+      <div className={styles.listContainer}>
+        <div className={styles.listTabs}>
+          {[Tab.Address, Tab.Transaction, Tab.Authorization].map(tab => (
+            <NavLink to={`${Routes.Main}/${tab}`} key={tab} activeClassName={styles.activeTab}>
+              {tab}
+            </NavLink>
+          ))}
+        </div>
+        <div className={styles.listBody}>
+          {list}
+        </div>
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default Main
